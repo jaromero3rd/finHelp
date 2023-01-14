63270241fb59b94fbefad3be20925f95f3ba5c85
Jaime Romero <jaromero3rd@gmail.com>
2023-01-14 13:32:23 -0500

    new server

--- client/script.js
+++ client/script.js
@@ -162,9 +162,8 @@ const handleSubmit = async (e) => {
 
 form.addEventListener('submit', handleSubmit)
 form.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13 || event.which === 13) {
         handleSubmit(e)
     }
-})
-form.addEventListener('unload', handleSubmit);
+});
 
