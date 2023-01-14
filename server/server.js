383f2bf6bbc3b3207f9bfb7be88db6bae5522fae
Jaime Romero <jaromero3rd@gmail.com>
2023-01-14 14:46:11 -0500

    new server

--- server/server.js
+++ server/server.js
@@ -25,7 +25,16 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.createCompletion({
+    if (prompt.length === 0){
+      res.status(200).send({
+        bot: 'Please enter a question!'
+      });
+    } else if (prompt.length < 5) {
+      res.status(200).send({
+        bot: 'Please enter a question!'
+      });
+    } else {
+      const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: isValidInput(`${prompt}`), //`${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
@@ -33,12 +42,7 @@ app.post('/', async (req, res) => {
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-    });
-    if (prompt.length < 5){
-      res.status(200).send({
-        bot: 'Please enter a question!'
       });
-    } else {
       res.status(200).send({
         bot: response.data.choices[0].text
       });
