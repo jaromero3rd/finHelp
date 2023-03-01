4c5181c2d3ce891004e7a7b62ad106445f1631b0
jaromero3rd <jaromero3rd@gmail.com>
2023-03-01 17:19:06 -0500

    new model

--- server/server.js
+++ server/server.js
@@ -35,8 +35,12 @@ app.post('/', async (req, res) => {
       });
     } else {
       const response = await openai.createCompletion({
-      model : "gpt-3.5-turbo",
-      prompt : [{role: "hacker", content:"you are a helpful hacker that would like to help the user by answering their questions"},{role: "user", content: `${prompt}`}], //`${prompt}`,
+        
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: "Hello world"}],
+        
+      // model : "gpt-3.5-turbo",
+      // prompt : [{role: "hacker", content:"you are a helpful hacker that would like to help the user by answering their questions"},{role: "user", content: `${prompt}`}], //`${prompt}`,
       // temperature: 0, // Higher values means the model will take more risks.
       // max_tokens: 100, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       // top_p: 1, // alternative to sampling with temperature, called nucleus sampling
