5c86c79d3e54393ea7fe5ae375403aa08bb72d0e
jaromero3rd <jaromero3rd@gmail.com>
2023-03-01 17:35:43 -0500

    new model

--- server/server.js
+++ server/server.js
@@ -39,18 +39,14 @@ app.post('/', async (req, res) => {
       });
     } else {
       const response = await openai.createCompletion({
-        
-      model: "gpt-3.5-turbo",
-      messages: [{role: "user", content: "Hello world"}],
-        
-      // model : "gpt-3.5-turbo",
-      // prompt : [{role: "hacker", content:"you are a helpful hacker that would like to help the user by answering their questions"},{role: "user", content: `${prompt}`}], //`${prompt}`,
-      // temperature: 0, // Higher values means the model will take more risks.
-      // max_tokens: 100, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      // top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      // frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      // presence_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-      });
+        model: "text-davinci-003",
+        prompt: isValidInput(`${prompt}`), //`${prompt}`,
+        temperature: 0, // Higher values means the model will take more risks.
+        max_tokens: 100, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+        top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+        frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+        presence_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+        });
       res.status(200).send({
         bot: response.data.choices[0].text
       });
@@ -65,7 +61,11 @@ app.post('/', async (req, res) => {
 function isValidInput(userInput) {
   const capitalizedUserIn =
     userInput[0].toUpperCase() + userInput.slice(1).toLowerCase();
-  return `${capitalizedUserIn}? `;
+  return `Bop: Hello User, I'm a financial advisor looking to help you save money.
+  User: Hello Bop, I am a person who would like to become more financially literate, and I have a few questions.
+  Bop: How can I help you? what is your question?
+  User: ${capitalizedUserIn}? 
+  `;
 }
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
