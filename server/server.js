ca42bbb50bcdb8500ab384943d98b8f265c46d1b
jaromero3rd <jaromero3rd@gmail.com>
2023-03-01 16:27:08 -0500

    new model

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.post('/', async (req, res) => {
       });
     } else {
       const response = await openai.createCompletion({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-0301",
       prompt: isValidInput(`${prompt}`), //`${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 100, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
@@ -57,8 +57,7 @@ app.post('/', async (req, res) => {
 function isValidInput(userInput) {
   const capitalizedUserIn =
     userInput[0].toUpperCase() + userInput.slice(1).toLowerCase();
-  return `${capitalizedUserIn}? 
-          `;
+  return `${capitalizedUserIn}? `;
 }
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
